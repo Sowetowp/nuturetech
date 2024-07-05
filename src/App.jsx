bd19355f1541e46583dd1c5f3d1292564f43a5ad
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-05 23:29:41 +0100

    auto-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 function App() {
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<}/>
+          <Route path='/' element={<Home/>}/>
+          
         </Routes>
       </Router>
     </>
