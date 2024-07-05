89f2707330c0254ddb4b6e8ba6ee997eeae50f70
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-05 23:54:43 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from "../assets/Frame 11.svg"
+import bell from "../assets/Frame 11.svg"
 import "../Styles/Style.css"
 import { Link } from 'react-router-dom'
 const Navbar = () => {
