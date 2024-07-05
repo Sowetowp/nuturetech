e26297b0eeca8b7821cc570becfb004dd8b0bf40
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-05 23:46:23 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <header className=''>
+            <header className='myHeader'>
 
             </header>
         </>
