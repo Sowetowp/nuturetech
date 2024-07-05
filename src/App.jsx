437fdd4ed7e96fa63e136acbd430938cfad8e2e9
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-05 23:48:13 +0100

    auto-commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import ApplicantDetails from './Pages/ApplicantDetails'
+import Layout from './Pages/Layout'
 function App() {
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Layout/>}/>
           <Route path='/applicantdetails' element={<ApplicantDetails/>}/>
         </Routes>
       </Router>
