ca071edb0ab129d3efb91cdfdf2ff6e4f24c0df1
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-06 00:03:16 +0100

    auto-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <>
             <header className='myHeader' style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
                 <div style={{width:"50%", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <img src={logo} alt="" style={{}}/>
+                    <img src={logo} alt="" style={{width:""}}/>
                     <span style={{display:"flex", gap:"10px"}}>
                         <Link href="" style={{textDecoration:"none", color:"#00E45B", fontSize:"12px"}}>Home</Link>
                         <Link href="" style={{textDecoration:"none", color:"#005B00", fontSize:"12px"}}>Companies</Link>
