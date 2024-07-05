7dd1f54cfc6aa9643e2ac189f670f6714c05ddd3
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-05 23:30:06 +0100

    auto-commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          
+          <Route path='/'/>
         </Routes>
       </Router>
     </>
