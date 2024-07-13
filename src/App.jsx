c9941e8925919d22ed18b3939e2f564ed9b48208
unknown <idrisrazakjibrin95@gmail.com>
2024-07-13 07:55:00 -0700

    anything

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import ApplicantDetails from './Pages/ApplicantDetails'
-import Layout from './Pages/Layout'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import ApplicantDetails from "./Pages/ApplicantDetails";
+import Layout from "./Pages/Layout";
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Layout/>}/>
-          <Route path='/applicantdetails' element={<ApplicantDetails/>}/>
+          <Route path="/" element={<Layout />} />
+          <Route path="/applicantdetails" element={<ApplicantDetails />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/ApplicantDetails.jsx
+++ src/Pages/ApplicantDetails.jsx
@@ -7,7 +7,7 @@ import bblogo from "../assets/bblogo.png";
 const ApplicantDetails = () => {
   return (
     <>
-      <Navbar />
+    <Navbar />
       <div className="container">
         <div className="contactInfo">
           <div className="facename">
@@ -24,53 +24,64 @@ const ApplicantDetails = () => {
         </div>
         <div className="contactprofile">
           <div>
-            <p>
-              Email address <br />
-              Bogdan@gmail.com
-            </p>
+            <p className="yyyy">Email address </p>
+            <h4>Bogdan@gmail.com</h4>
           </div>
           <hr className="hr-line" />
           <div>
-            <p>
-              phone number: <br /> (307) 555-0133
-            </p>
+            <p className="yyyy">phone number</p>
+            <h4>(307) 555-0133</h4>
           </div>
           <hr className="hr-line" />
           <div>
-            <p>
-              address: <br /> 2118 Thornridge Cir
-            </p>
+            <p className="yyyy">address </p>
+            <h4>2118 Thornridge Cir</h4>
           </div>
           <hr className="hr-line" />
           <div>
-            <p>
-              Skill level: <br />
-              Senior Level
-            </p>
+            <p className="yyyy">Skill level</p>
+            <h4>Senior Level</h4>
           </div>
         </div>
       </div>
       <div className="bandu">
-        <div className="bandc">
-          <div className="biography"><a href="Biography">Biography</a></div>
-          <div><a href="Job experience">Job Experience</a></div>
-          <div><a href="Skills">Skills</a></div>
-          <div><a href="Education">Education</a></div>
-          <div><a href="Portfolio">Portfolio</a></div>
-          <div><a href="Language">Language</a></div>
-          <div className="contact">
-            <div><a href="Contact">Contact</a></div>
-            <div><a href="Intrest">Intrest</a></div>
-            <div><a href="Recomendation">Recomendation</a></div></div>
+        <div className="bio">
+          <div className="biog">
+            <nav>
+              <ul>
+                <li><a href="">Biograghy</a></li>
+                <li><a href="">Job Experience</a></li>
+                <li><a href="">Skill</a></li>
+                <li><a href="">Education</a></li>
+                <li><a href="">Portfolio</a></li>
+                <li><a href="">Language</a></li>
+              </ul>
+            </nav>
           </div>
-          <div className="userinterface">
-            <div className="imb"><img src={bblogo} alt="" className="bblogo"/> 
-            <h4>User Interface Designer</h4><br />
-            <span>Bukalapak . jakarta, Indonesia</span></div>
-            <div><p>August 2017- june 2021</p></div>
-            <div className="writeup">Collaborate with product management and engineering to define and implement innovative solutions for the product direction, visuals and experience. Execute all visual design stages from concept to final hand-off to engineering. Conceptualize original ideas that bring simplicity and user friendliness to complex design roadblocks. Create wireframes, storyboards, user flows, process flows and site maps to effectively communicate interaction and design ideas. </div>
+          <div className="contact2">
+            <nav>
+              <ul>
+                <li><a href="">Contact</a></li>
+                <li><a href="">intrest</a></li>
+                <li><a href="">Recomendtion</a></li>
+              </ul>
+            </nav>
+
           </div>
         </div>
+        <div className="bio2">
+          <div className="bio3">
+          <div><img src= {bblogo} alt="" className="bblogo" />
+          <div className="bio4">
+          <h4>User Interface Designer</h4>
+          <p>Bukalapak . jakarta, Indonesia</p>
+          </div>
+          </div>
+          <div>August 2017- june 2021</div>
+          </div>
+        </div>
+        
+      </div>
     </>
   );
 };
